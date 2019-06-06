f0280f874e338193fd5b4f815274708538b0303e
lxlong <yunxu.it@outlook.com>
2019-06-06 10:47:45 +0800

    删除旧文件

--- src/MakeUtils.java
+++ src/MakeUtils.java
@@ -80,9 +80,24 @@ public class MakeUtils {
       fos.write(makeAllDimens(widthDp, designWidth, designHeight).getBytes());
       fos.flush();
       fos.close();
+      System.out.println("生成 " + file.getName() + " 成功");
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  protected static boolean deleteDir(File dir) {
+    if (dir.isDirectory()) {
+      String[] children = dir.list();
+      for (int i = 0; i < (children != null ? children.length : 0); i++) {
+        boolean success = deleteDir(new File(dir, children[i]));
+        if (!success) {
+          return false;
+        }
+      }
+    }
+    // 目录此时为空，可以删除
+    return dir.delete();
   }
 
 }
