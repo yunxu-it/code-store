bec11630433126969aa4614e3a7276fa2f66aaef
lxlong <yunxu.it@outlook.com>
2019-10-06 17:12:50 +0800

    feat: upgrade sw range

--- src/DimenGenerator.java
+++ src/DimenGenerator.java
@@ -5,14 +5,14 @@ public class DimenGenerator {
   /**
    * 设计稿尺寸(根据自己设计师的设计稿的宽度填入)
    */
-  private static final int DESIGN_WIDTH = 750;
-  private static final int DESIGN_HEIGHT = 1334;
+  private static final int DESIGN_WIDTH = 1080;
+  private static final int DESIGN_HEIGHT = 1920;
 
   /**
    * 适配Android 3.2以上   大部分手机的sw值集中在  300-460之间 sw = √(width^2 + height^2) / inch
    */
-  private static final int MIN_WIDTH_DP = 320;
-  private static final int MAX_WIDTH_DP = 460;
+  private static final int MIN_WIDTH_DP = 360;
+  private static final int MAX_WIDTH_DP = 500;
   private static final int STEP = 10;
   private static final String buildDir = "./out/res/";
 
