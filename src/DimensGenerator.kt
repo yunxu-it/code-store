a50c591b0e537327a46211e036bf46aa45d586df
lxlong <yunxu.it@outlook.com>
2020-12-18 15:27:00 +0800

    feat: 平板尺寸记录

--- src/DimensGenerator.kt
+++ src/DimensGenerator.kt
@@ -11,17 +11,17 @@ object DimensGenerator {
     /**
      * 适配Android 3.2以上   大部分手机的sw值集中在  300-460之间
      * 适配Android 3.2以上   大部分平板的sw值集中在  700-900之间
-     * 小米平板 1536x2048x7.9 758
-     * 小米4 1200x1920x8 678
-     * 华为平板 T1-A21W 800x1200x9.6 814
-     * 华为X6 800x1200x9.7 861
+     * 小米4 1200x1920x8 sw=678.4 dpi=283.02
+     * 小米平板 1536x2048x7.9 sw=758 dpi=324.05
+     * 华为平板 T1-A21W 800x1280x9.6 sw=814 dpi=157.23
+     * 华为X6 800x1280x9.7 sw=822.57 dpi=155.61
      *
      * dpi = √(width^2 + height^2) / inch
      * sw = height / (dpi /160 )
      * @see Calculator
      */
     private const val MIN_WIDTH_DP = 660
-    private const val MAX_WIDTH_DP = 960
+    private const val MAX_WIDTH_DP = 900
     private const val STEP = 20
     private const val buildDir = "./out/res/"
 
@@ -40,6 +40,7 @@ object DimensGenerator {
             MakeUtils.makeAll(DESIGN_WIDTH, if (isHorizontal) DESIGN_HEIGHT else DESIGN_WIDTH, i, buildDir)
             i += STEP
         }
+
     }
 
 
